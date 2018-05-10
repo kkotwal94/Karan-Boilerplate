c65518ed399a04cedcd308accfb0d2c048212114
Karan Kotwal <kotwalkaran64@gmail.com>
2018-05-10 00:42:33 -0400

    ASynchronous get for products

--- server/init/graphql.js
+++ server/init/graphql.js
@@ -1,6 +1,46 @@
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
-import schema from '../graph';
+import { buildSchema } from 'graphql';
+import ProductsModel from '../db/models/products';
+import { makeExecutableSchema } from 'graphql-tools';
+// import schema from '../graph';
+
+const products = [
+  {
+    id: '1',
+    style: 'G200',
+    description: 'A tshirt',
+  },
+  {
+    id: '2',
+    style: 'T200',
+    description: 'T200 shirt',
+  },
+];
+
+const typeDefs = `
+  type Product {
+    id: String,
+    style: String,
+    description: String,
+  }
+
+  type Query {
+    allProducts: [Product]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allProducts: async () => {
+      const products = await ProductsModel.find();
+      console.log(products);
+      return products;
+    },
+  },
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 export default app => {
   app.post(
